7b174e24ffbc7fa3308ee8498b9bbff3150399df
lkoteuka <llkot3ll@mail.ru>
2020-09-19 13:38:29 +0300

    Solution of homework01

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/mergeintervals/IntervalsMerger.java
@@ -1,5 +1,7 @@
 package io.github.javaasasecondlanguage.homework01.mergeintervals;
 
+import java.util.*;
+
 public class IntervalsMerger {
     /**
      * Given array of intervals, merge overlapping intervals and sort them by start in ascending order
@@ -16,6 +18,42 @@ public class IntervalsMerger {
      * @throws IllegalArgumentException if intervals is null
      */
     public int[][] merge(int[][] intervals) {
-        throw new RuntimeException("Not implemented");
+        if (intervals == null) {
+            throw new IllegalArgumentException();
+        }
+        if (intervals.length == 0) {
+            return new int[][]{};
+        }
+        Map<Integer, Integer> treeMap = new TreeMap<Integer, Integer>();
+        for (int i = 0; i < intervals.length; i++) {
+            treeMap.put(intervals[i][0], treeMap.getOrDefault(intervals[i][0], 0) + 1);
+            treeMap.put(intervals[i][1], treeMap.getOrDefault(intervals[i][1], 0) - 1);
+        }
+        List entryList = new ArrayList(treeMap.entrySet());
+
+        Integer counter = 0;
+        for (Object obj : entryList) {
+            counter += (Integer) ((Map.Entry) obj).getValue();
+            ((Map.Entry) obj).setValue(counter);
+        }
+        List<Integer> answ = new ArrayList<Integer>();
+        answ.add((Integer) ((Map.Entry) (entryList.get(0))).getKey());
+
+        for (int i = 0; i < entryList.size(); i++) {
+            if ((Integer) ((Map.Entry) entryList.get(i)).getValue() == 0) {
+                answ.add((Integer) ((Map.Entry) (entryList.get(i))).getKey());
+                if (i + 1 < entryList.size()) {
+                    answ.add((Integer) ((Map.Entry) (entryList.get(i + 1))).getKey());
+                }
+            }
+        }
+        int[][] answ_res = new int[answ.size() / 2][2];
+        int j = 0;
+        for (int i = 0; i < answ.size(); i += 2) {
+            answ_res[j][0] = answ.get(i);
+            answ_res[j][1] = answ.get(i + 1);
+            j++;
+        }
+        return answ_res;
     }
 }
