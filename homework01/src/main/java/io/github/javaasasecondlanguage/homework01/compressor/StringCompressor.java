089811adce7fca83b8fc7a97c218b007bf621f09
lkoteuka <llkot3ll@mail.ru>
2020-09-19 13:38:29 +0300

    Solution of homework01

--- homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
+++ homework01/src/main/java/io/github/javaasasecondlanguage/homework01/compressor/StringCompressor.java
@@ -1,9 +1,11 @@
 package io.github.javaasasecondlanguage.homework01.compressor;
 
+import java.util.Arrays;
+
 public class StringCompressor {
     /**
      * Given an array of characters, compress it using the following algorithm:
-     *
+     * <p>
      * Begin with an empty string s.
      * For each group of consecutive repeating characters in chars:
      * If the group's length is 1, append the character to s.
@@ -28,7 +30,41 @@ public class StringCompressor {
      * @throws IllegalArgumentException if str is null
      * @throws IllegalArgumentException if any char is not in range 'a'..'z'
      */
-    public char[] compress(char[] str) {
-        throw new RuntimeException("Not implemented");
+    public char[] compress(char[] str) throws IllegalArgumentException {
+        if (str == null) {
+            throw new IllegalArgumentException();
+        }
+        if (str.length == 0) {
+            return str;
+        }
+        int chang_ind = 0;
+        int acum = 0;
+        for (int i = 1; i < str.length; i++) {
+            while (str[i] == str[chang_ind]) {
+                if ((str[i] < 97) || (str[i] > 122)) {
+                    throw new IllegalArgumentException();
+                }
+                acum++;
+                i++;
+                if (i >= str.length) {
+                    break;
+                }
+            }
+            chang_ind++;
+            if (acum > 0) {
+                str[chang_ind] = Character.forDigit(acum + 1, 10);
+                chang_ind++;
+                if (i >= str.length) {
+                    break;
+                }
+                str[chang_ind] = str[i];
+                acum = 0;
+            } else {
+                str[chang_ind] = str[i];
+            }
+        }
+        if (acum == 0) chang_ind++;
+        char[] res = Arrays.copyOf(str, chang_ind);
+        return res;
     }
 }
