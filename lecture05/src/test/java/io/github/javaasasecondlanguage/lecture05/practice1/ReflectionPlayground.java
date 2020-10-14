9bfc51ebd35adaf54e30299360a8b13711e6467c
lkoteuka <llkot3ll@mail.ru>
2020-10-14 20:10:47 +0300

    Prac1

--- lecture05/src/test/java/io/github/javaasasecondlanguage/lecture05/practice1/ReflectionPlayground.java
+++ lecture05/src/test/java/io/github/javaasasecondlanguage/lecture05/practice1/ReflectionPlayground.java
@@ -3,30 +3,37 @@ package io.github.javaasasecondlanguage.lecture05.practice1;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Objects;
 
-@Disabled
+//@Disabled
 public class ReflectionPlayground {
     @Test
-    void createA() {
+    void createA() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
         // create A instance using reflection
         // hint: A.class
+        var a = A.class.getDeclaredConstructor().newInstance();
+        System.out.println(a);
     }
 
     @Test
-    void createB() {
+    void createB() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         // create B instance using reflection
         // hint: change access
+        var b = B.class.getDeclaredConstructor().newInstance();
     }
 
     @Test
     void listMethodsA() {
         // list methods of class A
+        System.out.println(Arrays.toString(A.class.getMethods()));
     }
 
     @Test
     void listMethodsAnnotatedWithOverride() {
         // list Methods of class C annotated with override
+        System.out.println(Arrays.toString(C.class.getDeclaredMethods()));
     }
 
     static class A {
